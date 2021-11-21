e636efcc0f2929ebad03db8713fb00b4392e4f6a
cat <nobody@email.com>
2021-11-21 16:39:28 -0600

    : n args test

--- src/StringCalculator/StringCalculatorTest.java
+++ src/StringCalculator/StringCalculatorTest.java
@@ -13,40 +13,60 @@ public class StringCalculatorTest {
 
         assertEquals(0, sum);
     }
+
     @Test
     public void when1_ret1() {
         int sum = calc.add("1");
 
         assertEquals(1, sum);
     }
+
     @Test
     public void when2_ret2() {
         int sum = calc.add("2");
 
         assertEquals(2, sum);
     }
+
     @Test
     public void whenN_retN() {
         int sum = calc.add("4324");
 
         assertEquals(4324, sum);
     }
+
     @Test
     public void when2Args_retSum() {
         int sum = calc.add("1,2");
 
         assertEquals(3, sum);
     }
+
     @Test
     public void when2Args_retSum_2() {
         int sum = calc.add("0,5");
 
         assertEquals(5, sum);
     }
+
     @Test
     public void when3Args_retSum() {
         int sum = calc.add("1,5,7");
 
         assertEquals(1 + 5 + 7, sum);
+    }
+
+    @Test
+    public void when4Args_retSum() {
+        int sum = calc.add("1,5,7,143");
+
+        assertEquals(1 + 5 + 7 + 143, sum);
+    }
+
+    @Test
+    public void whenNArgs_retSum() {
+        int sum = calc.add("5,23,56,21,645,43");
+
+        assertEquals(5 + 23 + 56 + 21 + 645 + 43, sum);
     }
 }
