13c303e09cbd7675a94869b43cf5d7f72473fc92
cat <nobody@email.com>
2021-11-21 16:44:33 -0600

    : refactor

--- src/StringCalculator/StringCalculator.java
+++ src/StringCalculator/StringCalculator.java
@@ -4,10 +4,18 @@ import java.util.Arrays;
 
 public class StringCalculator {
     public int add(String s) {
-        String[] args = s.split("(,|\\n)");
+        String[] args = parseInput(s);
+        return getSum(args);
+    }
+
+    private int getSum(String[] args) {
         return Arrays.stream(args)
                 .filter(a -> !a.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    private String[] parseInput(String s) {
+        return s.split("(,|\\n)");
     }
 }
