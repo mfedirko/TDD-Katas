f12941c631a5f81fed53007c604bcbab83f5561d
cat <nobody@email.com>
2021-11-21 17:17:36 -0600

    : refactor

--- src/StringCalculator/InputParser.java
+++ src/StringCalculator/InputParser.java
@@ -3,6 +3,8 @@ package StringCalculator;
 import java.util.Arrays;
 
 class InputParser {
+    private static final String DEFAULT_DELIMITER = "(,|\\n)";
+
     private final String s;
 
     InputParser(String s) {
@@ -10,14 +12,18 @@ class InputParser {
     }
 
     int[] parse() {
-        String delim = "(,|\\n)";
-        String nums = s;
+        String delim = DEFAULT_DELIMITER;
+        String numPart = s;
         if (hasCustomDelimiter(s)) {
             final int endOfDelim = s.indexOf('\n');
             delim = s.substring(2,endOfDelim);
-            nums = s.substring(1 + endOfDelim);
+            numPart = s.substring(1 + endOfDelim);
         }
-        return Arrays.stream(nums.split(delim))
+        return getNumbers(delim, numPart);
+    }
+
+    private int[] getNumbers(String delim, String numPart) {
+        return Arrays.stream(numPart.split(delim))
                 .filter(a -> !a.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .toArray();
